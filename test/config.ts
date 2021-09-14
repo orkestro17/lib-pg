893fef7859c0aca57f38d4c008640fe32c863c3c
GPad <gpadovani@gmail.com>
2021-09-14 13:20:58 +0200

    Read actual env

--- test/config.ts
+++ test/config.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg"
 import { getPgConfig } from "../src/config"
 
-export const testConfig = getPgConfig({ database: "pg_lib_test" })
+export const testConfig = getPgConfig(process.env)
 export const testPool = new Pool({ ...testConfig, min: 0, max: 1 })
 
 console.log(">>> ENV ", process.env)
