69cefea7b5ba15ffa3e3209e6675fa1f58aba699
GPad <gpadovani@gmail.com>
2021-09-14 14:33:59 +0200

    Remove useless log

--- test/config.ts
+++ test/config.ts
@@ -4,9 +4,6 @@ import { getPgConfig } from "../src/config"
 export const testConfig = getPgConfig(process.env)
 export const testPool = new Pool({ ...testConfig, min: 0, max: 1 })
 
-console.log(">>> ENV ", process.env)
-console.log(">>> TestConfig ", testConfig)
-
 testPool.on("error", (e) => {
   console.error("Error happened in test db pool")
   console.error(e)
